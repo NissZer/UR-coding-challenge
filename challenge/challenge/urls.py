82ebbf09d4f0dcfe3c0411476ed08c98e0fc5676
Nisrine Zerouali <nisszerouali@gmail.com>
2019-05-30 17:36:30 +0000

    Include urls under django.contrib.auth.

--- challenge/challenge/urls.py
+++ challenge/challenge/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^login/', include('login.urls')),
-
+    path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls'))
 ]
